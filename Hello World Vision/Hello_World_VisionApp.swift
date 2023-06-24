630125ba8dd88d09caa55d9325d98ac164823951
Bram-000 <dark.gige@gmail.com>
2023-06-24 13:39:42 +0200

    Initial Commit

---
+++ Hello World Vision/Hello_World_VisionApp.swift
@@ -0,0 +1,17 @@
+//
+//  Hello_World_VisionApp.swift
+//  Hello World Vision
+//
+//  Created by Andrei Ramescu on 24/06/23.
+//
+
+import SwiftUI
+
+@main
+struct Hello_World_VisionApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
